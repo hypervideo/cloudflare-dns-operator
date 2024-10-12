4acd63f8503016156da9beb55ebb21582ea1ee57
Robert Krahn <robert.krahn@gmail.com>
2024-10-12 03:00:35 +0200

    nix package

--- flake.nix
+++ flake.nix
@@ -6,31 +6,51 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
-      in
-        {
-          devShells.default = pkgs.mkShell {
-            nativeBuildInputs = with pkgs; [
-              rustc
-              cargo
-              clippy
-              clang
-              pkg-config
-            ];
+      let
+        pkgs = import nixpkgs { inherit system; };
+        lib = pkgs.lib;
 
-            buildInputs = with pkgs; [
-              openssl
-            ];
+        cloudflare-dns-operator = pkgs.rustPlatform.buildRustPackage {
+          pname = "cloudflare-dns-operator";
+          version = "0.1.0";
 
-            packages = with pkgs; [
-              rust-analyzer
-              (rustfmt.override { asNightly = true; })
-            ];
+          cargoHash = "sha256-d2/RG2ZHvxhFkkUQFwJDLwhWjp8E27Hq4Nm9WlqWhY4=";
+          src = ./.;
 
-            RUST_BACKTRACE = "1";
-            RUST_LOG = "debug,hyper_util=info,tower=info,rustls=info,kube=info";
-            LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+          meta = with lib; {
+            homepage = "https://github.com/hypervideo/cloudflare-dns-operator";
+            description = "This is a kubernetes operator to manage cloudflare DNS entries from within kubernetes.";
+            mainProgram = "cloudflare-dns-operator";
+            maintainers = with maintainers; [ rksm ];
           };
-        }
+        };
+
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            rustc
+            cargo
+            clippy
+            clang
+            pkg-config
+          ];
+
+          buildInputs = with pkgs; [
+            openssl
+          ];
+
+          packages = with pkgs; [
+            rust-analyzer
+            (rustfmt.override { asNightly = true; })
+          ];
+
+          RUST_BACKTRACE = "1";
+          RUST_LOG = "debug,hyper_util=info,tower=info,rustls=info,kube=info";
+          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+        };
+
+        packages = { inherit cloudflare-dns-operator; };
+      }
     );
 }
