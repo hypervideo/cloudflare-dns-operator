7cf0a416e6a202f9c45848edf8df0d0ee7388d37
Robert Krahn <robert.krahn@gmail.com>
2024-10-13 04:53:33 +0200

    license

--- nix/package.nix
+++ nix/package.nix
@@ -14,6 +14,7 @@ rustPlatform.buildRustPackage {
     homepage = "https://github.com/hypervideo/cloudflare-dns-operator";
     description = "This is a kubernetes operator to manage cloudflare DNS entries from within kubernetes.";
     mainProgram = "cloudflare-dns-operator";
+    license = licenses.mpl20;
     maintainers = with maintainers; [ rksm ];
   };
 }
